52e302fe344b749bfd25baa1490b75951f4fa66a
Brandon Dickson <bdickson1212@gmail.com>
2020-11-14 14:28:52 +0000

    added logic for if no answer is inputted

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      'You need to enter a number! 😄';
+  }
+});
