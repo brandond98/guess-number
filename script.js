b0817bac7baf0c533e29b1af5a41ebf326e0d7f4
Brandon Dickson <bdickson1212@gmail.com>
2020-11-14 15:18:58 +0000

    added some comments

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent =
       'You need to enter a number! 😄';
+
+    // When the player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent =
       'You entered the correct number! 🎉';
+
+    // When the guess it too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high, try again!';
@@ -22,6 +27,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = '0';
     }
+
+    // When the guess it too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low, try again!';
